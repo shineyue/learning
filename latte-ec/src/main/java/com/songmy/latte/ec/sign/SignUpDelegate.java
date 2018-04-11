84a27296227d4f69f005f2bcb6dd7f4a882892b4
1996554 <aliheyue@126.com>
2018-04-11 19:16:33 +0800

    temp

---
+++ latte-ec/src/main/java/com/songmy/latte/ec/sign/SignUpDelegate.java
@@ -0,0 +1,25 @@
+package com.songmy.latte.ec.sign;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.songmy.latte.delegates.LatteDelegate;
+
+/**
+ * Created by songmy on 2018/4/11.
+ */
+
+public class SignUpDelegate extends LatteDelegate{
+
+    @Override
+    public Object setLayout() {
+        return null;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+
+    }
+
+}
